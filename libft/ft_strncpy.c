6157119ea5c26e174b6c34aa1dd48a2f9d8445d1
Winston Weathers <wiweathe@e1z3r7p2.42.us.org>
2018-04-24 00:41:30 -0700

    fix

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -6,23 +6,24 @@
 /*   By: wiweathe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 19:45:06 by wiweathe          #+#    #+#             */
-/*   Updated: 2018/04/23 19:48:06 by wiweathe         ###   ########.fr       */
+/*   Updated: 2018/04/24 00:40:45 by wiweathe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	ft_strncpy(char *dst, const char *src, size_t len)
+char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	size_t	i;
 
 	i = 0;
-	while (src[i] && i < n)
+	len = 0;
+	while (src[i] != '\0' || len < i)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	while (i < n)
+	if (src[i] == '\0' && len < i)
 	{
 		dst[i] = '\0';
 		i++;
